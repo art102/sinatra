79b085647f7d03d616acebd513e8fef10d8d17c3
ArturDaukaev <daukaevar@bashneft.ru>
2017-07-05 13:28:58 +0500

    Add username, phone and date into file

--- barbershop/barbershop.rb
+++ barbershop/barbershop.rb
@@ -5,6 +5,7 @@ get '/' do
 end
 
 post '/' do
+	# Присваиваем переменным значения введенные в форму
 	@name = params[:user_name]
 	@phone = params[:phone]
 	@date = params[:date_time]
@@ -12,5 +13,10 @@ post '/' do
 	@title = 'Thank you!'
 	@message = "Dear #{@name}, we'll be waitng for you at #{@date}"
 
+	# Добавляем имя, телефон и дату в файл users.txt и закрываем файл
+	file = File.open('users.txt', 'a')
+	file.write("Name:#{@name}, Phone:#{@phone}, Date and time:#{@date}\n")
+	file.close
+	
 	erb :message
 end
